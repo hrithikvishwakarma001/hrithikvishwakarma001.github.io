1b04ad12d917a6934464c816bd55cef9179dd344
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-07-10 00:13:52 +0530

    add some recent projects

--- src/utils/ScrollContainer.jsx
+++ src/utils/ScrollContainer.jsx
@@ -25,71 +25,23 @@ import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { GoTools } from "react-icons/go";
 const projects = [
 	{
-		id: 7,
-		tools: "Javascript | React JS | Chakra UI",
-		tag: "Make-up Products site",
-		span: "5 days",
-		week: "Clone of nykaa.com",
-		img: "mykaa.png",
-		typePlan: "Mykaa",
-		github: "https://github.com/hrithikvishwakarma001/bitter-level-627.git",
-		link: "https://www.nykaa.com/?root=logo",
-		deploy: "https://mykaa.vercel.app/",
-	},
-	{
 		id: 1,
-		tools: "Typescript | React JS | Chakra UI",
-		tag: "Home decor site",
-		span: "5 days",
-		week: "Clone of dollartree.com",
-		img: "deco.jpg",
-		typePlan: "Deco-ninja",
-		github: "https://github.com/hrithikvishwakarma001/potent-scent-4991.git",
-		link: "https://www.dollartree.com/",
-		deploy: "https://deco-ninja.vercel.app",
+		tools: "HTML | CSS | JavaScript",
+		tag: "Ecommerce",
+		span: "2 days",
+		website: "Grill In",
+		img: "grill.png",
+		typePlan: "Grill In",
+		github: "https://github.com/hrithikvishwakarma001/Grill-inn",
+		link: "https://shiny-cranachan-408e6a.netlify.app/",
+		deploy: "https://shiny-cranachan-408e6a.netlify.app/",
 	},
 	{
 		id: 2,
-		tools: "Javascript | React JS | Chakra UI",
-		tag: "Booking site",
-		span: "5 days",
-		week: "Clone of avis.com",
-		img: "rentovento.png",
-		typePlan: "Rentovento",
-		github: "https://github.com/hrithikvishwakarma001/unkempt-snow-4804",
-		link: "https://www.avis.com/en/home",
-		deploy: "https://rentovento.vercel.app",
-	},
-	{
-		id: 3,
-		tools: "JavaScript",
-		tag: "Snippets-extension",
-		span: "3 days",
-		week: "Extension marketplace",
-		img: "snippets.png",
-		typePlan: "snippets",
-		github: "https://github.com/hrithikvishwakarma001/snippets-extension",
-		link: "https://marketplace.visualstudio.com/items?itemName=hrithikvishwakarma001.fetch-axios-snippets",
-		deploy: "https://marketplace.visualstudio.com/items?itemName=hrithikvishwakarma001.fetch-axios-snippets",
-	},
-	{
-		id: 4,
 		tools: "HTML | CSS | JavaScript",
 		tag: "Ecommerce",
 		span: "5 days",
-		week: "Clone of mythrersa.com",
-		img: "clothingo.png",
-		typePlan: "Clothingo",
-		github: "https://github.com/hrithikvishwakarma001/complex-oranges-7381",
-		link: "https://www.mytheresa.com/int_en/women.html",
-		deploy: "https://luxury-vacherin-56d222.netlify.app/",
-	},
-	{
-		id: 5,
-		tools: "HTML | CSS | JavaScript",
-		tag: "Ecommerce",
-		span: "5 days",
-		week: "Clone of geekbuying.com",
+		website: "Clone of geekbuying.com",
 		img: "techx.png",
 		typePlan: "Tech-X",
 		github: "https://github.com/hrithikvishwakarma001/lackadaisical-look-2573",
@@ -97,19 +49,103 @@ const projects = [
 		deploy: "https://chimerical-mousse-3a41fa.netlify.app/",
 	},
 	{
-		id: 6,
+		id: 3,
 		tools: "HTML | CSS | JavaScript",
 		tag: "Ecommerce",
+		span: "5 days",
+		website: "Clone of mythrersa.com",
+		img: "clothingo.png",
+		typePlan: "Clothingo",
+		github: "https://github.com/hrithikvishwakarma001/complex-oranges-7381",
+		link: "https://www.mytheresa.com/int_en/women.html",
+		deploy: "https://luxury-vacherin-56d222.netlify.app/",
+	},
+	{
+		id: 4,
+		tools: "JavaScript",
+		tag: "Snippets-extension",
+		span: "3 days",
+		website: "Extension marketplace",
+		img: "snippets.png",
+		typePlan: "snippets",
+		github: "https://github.com/hrithikvishwakarma001/snippets-extension",
+		link: "https://marketplace.visualstudio.com/items?itemName=hrithikvishwakarma001.fetch-axios-snippets",
+		deploy: "https://marketplace.visualstudio.com/items?itemName=hrithikvishwakarma001.fetch-axios-snippets",
+	},
+	{
+		id: 5,
+		tools: "Javascript | React JS | Chakra UI",
+		tag: "Booking site",
+		span: "5 days",
+		website: "Clone of avis.com",
+		img: "rentovento.png",
+		typePlan: "Rentovento",
+		github: "https://github.com/hrithikvishwakarma001/unkempt-snow-4804",
+		link: "https://www.avis.com/en/home",
+		deploy: "https://rentovento.vercel.app",
+	},
+	{
+		id: 6,
+		tools: "Typescript | React JS | Chakra UI",
+		tag: "Home decor site",
+		span: "5 days",
+		website: "Clone of dollartree.com",
+		img: "deco.jpg",
+		typePlan: "Deco-ninja",
+		github: "https://github.com/hrithikvishwakarma001/potent-scent-4991.git",
+		link: "https://www.dollartree.com/",
+		deploy: "https://deco-ninja.vercel.app",
+	},
+	{
+		id: 7,
+		tools: "Javascript | React JS | Chakra UI",
+		tag: "Make-up Products site",
+		span: "5 days",
+		website: "Clone of nykaa.com",
+		img: "mykaa.png",
+		typePlan: "Mykaa",
+		github: "https://github.com/hrithikvishwakarma001/bitter-level-627.git",
+		link: "https://www.nykaa.com/?root=logo",
+		deploy: "https://mykaa.vercel.app/",
+	},
+	{
+		id: 8,
+		tools: "React JS | Node JS | MognoDB | Express JS",
+		tag: "Clothings fashion site",
+		span: "5 days",
+		website: "Clone of koovs.com",
+		img: "clotho.png",
+		typePlan: "Clotho",
+		github: "https://github.com/hrithikvishwakarma001/Koovs-clone-Clotho",
+		link: "https://www.koovs.com/",
+		deploy: "https://clotho.vercel.app/",
+	},
+	{
+		id: 9,
+		tools: "React JS | Node JS | TypeScript | Tailwind CSS",
+		tag: "Fitness site (Hackathon)",
+		span: "3 days",
+		website: "Self designed project",
+		img: "fitfustion.png",
+		typePlan: "Fitfusion",
+		github: "https://github.com/hrithikvishwakarma001/fitfusion-hackathon",
+		link: "https://fitfusion.vercel.app/",
+		deploy: "https://fitfusion.vercel.app/",
+	},
+	{
+		id: 10,
+		tools: "Javascript | React JS | Chakra UI",
+		tag: "VidsCoin (Company task)",
 		span: "2 days",
-		week: "Grill In",
-		img: "grill.png",
-		typePlan: "Grill In",
-		github: "https://github.com/hrithikvishwakarma001/Grill-inn",
-		link: "https://shiny-cranachan-408e6a.netlify.app/",
-		deploy: "https://shiny-cranachan-408e6a.netlify.app/",
+		website: "Figma to React JS",
+		img: "vidscoin.png",
+		typePlan: "VidsCoin",
+		github: "https://github.com/hrithikvishwakarma001/vidscoin",
+		link: "https://www.figma.com/file/e08uWkzw9Vk5PVszbrrdKi/Vidscoin?type=design&node-id=0-1&mode=design",
+		deploy: "https://vidscoin.vercel.app/",
 	},
 ];
-
+const reverseProjects = projects.reverse();
 const PackageTier = ({
 	img,
 	typePlan,
@@ -117,7 +153,7 @@ const PackageTier = ({
 	deploy,
 	tag,
 	span,
-	week,
+	website,
 	link,
 	tools,
 }) => {
@@ -147,19 +183,19 @@ const PackageTier = ({
 			alignItems={{ md: "center" }}>
 			<Image
 				src={img}
-				w={{base:"100%",md:"20rem"}}
-				h="185px"
+				w={{ base: "100%", md: "20rem" }}
+				h='185px'
 				objectFit={"cover"}
 				alt={typePlan}
 				border='2px dashed gray'
-				p="2"
+				p='2'
 			/>
 
 			<List spacing={3} textAlign='start' className='project-tech-stack'>
 				<ListItem display='flex' alignItems={"center"}>
 					<ListIcon as={ExternalLinkIcon} color='green.500' />
 					<a href={link} target='_blank' rel='noreferrer'>
-						{week}
+						{website}
 					</a>
 				</ListItem>
 				<ListItem
@@ -185,7 +221,7 @@ const PackageTier = ({
 				fontSize={"1rem"}
 				fontWeight='400'
 				px='2'
-				py="2"
+				py='2'
 				colorScheme='purple'
 				textAlign='start'
 				maxW={{ base: "100%", md: "325px" }}
@@ -237,12 +273,12 @@ export default function ScrollContainer() {
 		<Container maxW={{ base: "100%", md: "100%", lg: "100%" }}>
 			<Swiper
 				direction={smallScreen ? "horizontal" : "vertical"}
-				slidesPerView={smallScreen?1:2}
+				slidesPerView={smallScreen ? 1 : 2}
 				spaceBetween={20}
 				mousewheel={true}
 				modules={[Mousewheel]}
 				className='mySwiper'>
-				{projects.map((project) => (
+				{reverseProjects.map((project) => (
 					<SwiperSlide>
 						<PackageTier {...project} />
 					</SwiperSlide>
@@ -251,3 +287,5 @@ export default function ScrollContainer() {
 		</Container>
 	);
 }
+
+
